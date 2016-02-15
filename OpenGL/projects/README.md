ac546050b9cf5b2920cc392751e862a7c11450e7
JangoBoogaloo <jango.boogaloo@gmail.com>
2016-02-15 11:01:18 -0500

    add new opengl tutorial material

--- OpenGL/projects/README.md
+++
@@ -1,1 +0,0 @@
-Projects supported by freeglut 2.8
--- OpenGL/projects/win/q2/Q/viewer/DummySwing.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-extern float g_headpos[];
-extern float g_grippos[];
-extern float g_quaternion[];
-
-#define DUMMY_HEAD_POS_LEN 1930
-#define DUMMY_GRIP_POS_LEN 1930
-#define DUMMY_QUATERNION_LEN 2008
