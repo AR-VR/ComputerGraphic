308a98582837ec2c5792fcc3455a6073fe107d80
JangoBoogaloo <jango.boogaloo@gmail.com>
2016-02-24 22:04:45 -0500

    playground add shader and matrices

--- OpenGL/ogl-OpenGL-tutorial_0015_33/playground/playground.cpp
+++ OpenGL/ogl-OpenGL-tutorial_0015_33/playground/playground.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 
 #include <GL/glew.h>
 
@@ -7,7 +8,12 @@
 GLFWwindow* window;
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <common/shader.hpp>
+
 using namespace glm;
+using namespace std;
 
 int main( void )
 {
@@ -20,7 +26,6 @@ int main( void )
 	}
 
 	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
@@ -37,6 +42,7 @@ int main( void )
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
+    glewExperimental = true; // Needed for core profile
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -49,10 +55,77 @@ int main( void )
 
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    
+    //////////////////////////////////////////////////////////////
+    static const int kTriangleSize = 3;
+    vector<GLuint> vertexArrayIDs(kTriangleSize);
+    
+    GLuint arrayID;
+    glGenVertexArrays(1, &arrayID);
+    for(int i=0; i<vertexArrayIDs.size(); i++)
+    {
+        GLuint arrayID;
+        glGenVertexArrays(1, &arrayID);
+        vertexArrayIDs[i] = arrayID;
+        glBindVertexArray(vertexArrayIDs[i]);
+    }
+    
+    GLuint programID = LoadShaders("Vertex.vert", "Fragment.frag");
+    //////////////////////////////////////////////////////////////
+    
+    // Get a handle for our "MVP" uniform
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+    glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+    // Camera matrix
+    glm::mat4 View       = glm::lookAt(glm::vec3(6,3,3), // Camera is at (4,3,3), in World Space
+                                       glm::vec3(0,0,0), // and looks at the origin
+                                       glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+                                       );
+    glm::mat4 Model      = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+    
+    vector<GLuint> vertexBuffers(kTriangleSize);
+    for(int i=0; i<vertexBuffers.size(); i++)
+    {
+        GLfloat vertexBufferData [] = {
+            -1.0f, -1.0f, i,
+            1.0f, -1.0f, i,
+            0.0f,  1.0f, i,
+        };
+        GLuint bufferID;
+        glGenBuffers(1, &bufferID);
+        vertexBuffers[i] = bufferID;
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffers[i]);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertexBufferData), vertexBufferData, GL_STATIC_DRAW);
+    }
 
 	do{
-		// Draw nothing, see you in tutorial 2 !
-
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        glUseProgram(programID);
+        // Send our transformation to the currently bound shader,
+        // in the "MVP" uniform
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+        
+        // 1rst attribute buffer : vertices
+        for(int i=0; i<vertexBuffers.size(); i++)
+        {
+            glEnableVertexAttribArray(i);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffers[i]);
+            glVertexAttribPointer(
+                0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                3,                  // size
+                GL_FLOAT,           // type
+                GL_FALSE,           // normalized?
+                0,                  // stride
+                (void*)0            // array buffer offset
+            );
+            // Draw the triangle !
+            glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+            glDisableVertexAttribArray(i);
+        }
+        
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -61,6 +134,16 @@ int main( void )
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
 
+    glDeleteProgram(programID);
+
+    // Cleanup VBO
+    for(int i=0; i<vertexBuffers.size(); i++)
+    {
+        glDeleteBuffers(1, &vertexBuffers[i]);
+        glDeleteVertexArrays(1, &vertexArrayIDs[i]);
+    }
+    
+    
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
 
