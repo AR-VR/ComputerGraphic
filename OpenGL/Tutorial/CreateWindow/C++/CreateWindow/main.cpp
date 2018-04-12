098694c294e75469fe793523d3a90a5615dd5934
JangoBoogaloo <jango.boogaloo@gmail.com>
2018-04-11 21:08:30 -0400

    Draw triangle - without EBO

--- OpenGL/Tutorial/CreateWindow/C++/CreateWindow/main.cpp
+++ OpenGL/Tutorial/CreateWindow/C++/CreateWindow/main.cpp
@@ -16,17 +16,19 @@ const unsigned int SCR_HEIGHT = 600;
 
 const GLchar* const vertexShaderSrc =
 "#version 330 core																							\n"
-"layout (location = 0) in vec3 position;												\n"
+"layout (location = 0) in vec3 position;												\n" //This define position variable at vertex attribute pointer index 0
 "void main() {																									\n"
-"  gl_Position = vec4(position.x, position.y, position.z, 1.0); \n"
+"  gl_Position = vec4(position.x, position.y, position.z, 1.0); \n" //Convert vec3 position to vec4 position (eventually for w division?)
 "}";
 
 const GLchar* const fragmentShaderSrc =
 "#version 330 core																							\n"
 "out vec4 FragColor;																						\n"
 "void main() {																									\n"
-"  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);										\n"
+"  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);										\n" //Set all the drawing to this color
 "}";
+
+
 
 int main(int argc, char** argv) {
 	glfwInit();
@@ -60,7 +62,8 @@ int main(int argc, char** argv) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	
+
+	//Init GL program
 	GLuint vertexShader;
 	GL_EXEC(vertexShader = glCreateShader(GL_VERTEX_SHADER));
 	GL_EXEC(glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL));
@@ -80,6 +83,50 @@ int main(int argc, char** argv) {
 	GL_EXEC(glDeleteShader(vertexShader));
 	GL_EXEC(glDeleteShader(fragmentShader));
 
+
+
+	//
+	//     1
+	// -1     1
+	//    -1
+
+	//		2
+	//    
+	// 0     1
+	//
+	const float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	const unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 2  // first Triangle
+		//1, 2, 3   // second Triangle
+	};
+
+	unsigned int vao, vbo;
+	GL_EXEC(glGenVertexArrays(1, &vao));
+	GL_EXEC(glGenBuffers(1, &vbo));
+
+	//1. Bind vertex array object (container) first
+	GL_EXEC(glBindVertexArray(vao));
+	
+	//2. Bind VBO
+	GL_EXEC(glBindBuffer(GL_ARRAY_BUFFER, vbo));
+
+	//Load vertices data to GPU, and GL_STATIC_DRAW means data will not be modified after loading
+	GL_EXEC(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
+
+	//3. Configure vertex attributes (bind to shader variable from my understanding)
+	int vertexIndex = 0;
+	GL_EXEC(glVertexAttribPointer(vertexIndex, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0));
+	GL_EXEC(glEnableVertexAttribArray(vertexIndex));
+	//4. Unbind VBO, prevent overwritten/polluted
+	GL_EXEC(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	//5. unbind VAO, not a must
+	GL_EXEC(glBindVertexArray(0));
+
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -87,12 +134,22 @@ int main(int argc, char** argv) {
 		// input
 		// -----
 		processInput(window);
-		
+
+		GL_EXEC(glClearColor(0.2f, 0.3f, 0.3f, 1.0f));
+		GL_EXEC(glClear(GL_COLOR_BUFFER_BIT));
+
+		GL_EXEC(glUseProgram(glProgram));
+		GL_EXEC(glBindVertexArray(vao));
+		GL_EXEC(glDrawArrays(GL_TRIANGLES, 0, 3));
+		GL_EXEC(glBindVertexArray(0));
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	GL_EXEC(glDeleteBuffers(1, &vbo));
+	GL_EXEC(glDeleteVertexArrays(1, &vao));
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
