0aa483a3609c5d233264a9663a9c0f82b905379c
JangoBoogaloo <jango.boogaloo@gmail.com>
2018-10-31 21:50:32 -0400

    Disable OpenGL version hint (3.3 may not be supported)

--- OpenGL/Tutorial/CreateWindow/C++/CreateWindow/main.cpp
+++ OpenGL/Tutorial/CreateWindow/C++/CreateWindow/main.cpp
@@ -29,6 +29,7 @@ static Cube cube;
 
 static float zNear = 0.1, zFar = 99.0;
 static float fovYDegree = 60;
+
 int main(int argc, char** argv) {
 
 #ifndef _DEBUG
@@ -36,10 +37,6 @@ int main(int argc, char** argv) {
 #endif
 
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 
 #ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
